56965d528cc7a327151b50fe3d3037ba8b5ec146
Andrey Prez <aw06.1994@yandex.ru>
2023-04-04 08:17:27 +0800

    Main screen created

---
+++ GuessTheNumber/GuessTheNumber/Cells/BallCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BallCollectionViewCell.swift
+//  GuessTheNumber
+//
+//  Created by Андрей  on 04.04.2023.
+//
+
+import UIKit
+
+class BallCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ GuessTheNumber/GuessTheNumber/Cells/NumberCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NumberCollectionViewCell.swift
+//  GuessTheNumber
+//
+//  Created by Андрей  on 04.04.2023.
+//
+
+import UIKit
+
+class NumberCollectionViewCell: UICollectionViewCell {
+    
+}
